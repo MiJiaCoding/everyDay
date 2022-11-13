59169c4e0fd3f52f7041b1dd4fa60bb8d863412e
mijia <1137551712@qq.com>
2022-11-13 21:55:50 +0800

    everyday

---
+++ sx09.new.js
@@ -0,0 +1,11 @@
+const _new = function(constructor,...args) {
+    // 补全代码
+    //1.定义一个空对象
+    const obj={}
+    //2.隐式原型指向构造函数的显示原型
+    obj.__proto__=constructor.prototype
+    //3.执行构造函数,this指向空对象
+    constructor.apply(obj,args)
+    //4.返回对象
+    return obj
+}
