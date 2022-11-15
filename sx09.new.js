1fae87b2ba97aa2aee08cae5730d1b3f2f9a1e5c
mijia <1137551712@qq.com>
2022-11-15 21:35:30 +0800

    everyday

--- sx09.new.js
+++ sx09.new.js
@@ -9,3 +9,14 @@ const _new = function(constructor,...args) {
     //4.返回对象
     return obj
 }
+
+function Person(b){
+    this.a=1
+    this.func=()=>{
+        console.log(this.a,b)
+    }
+}
+
+const obj =_new(Person,123)
+console.log(obj.a)
+obj.func()
